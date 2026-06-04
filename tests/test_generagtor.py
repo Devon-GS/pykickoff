967934848a80963ed7776896f0aea08beed051bb
Devon <ds.pydev@gmail.com>
2026-06-04 14:02:38 +0200

    Add testing and update pyproject.toml

--- tests/test_generagtor.py
+++ tests/test_generagtor.py
@@ -1,15 +1,15 @@
-from pykickoff import ProjectGenerator
+# from pykickoff import ProjectGenerator
 
-# Simulate the data we'd get from the user questions
-user_input = {
-	"project_name": "my-cool-app",
-	"description": "A weather tracking app.",
-	"author_name": "Your Name",
-	"is_cli": True,
-	"init_git": True,
-	"create_venv": True
-}
+# # Simulate the data we'd get from the user questions
+# user_input = {
+# 	"project_name": "my-cool-app",
+# 	"description": "A weather tracking app.",
+# 	"author_name": "Your Name",
+# 	"is_cli": True,
+# 	"init_git": True,
+# 	"create_venv": True
+# }
 
-# Run the generator
-gen = ProjectGenerator(user_input)
-gen.run()
+# # Run the generator
+# gen = ProjectGenerator(user_input)
+# gen.run()
